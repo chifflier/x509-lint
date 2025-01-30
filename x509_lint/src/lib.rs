135e250b7922067d064aef23210eb6bdb29d4b28
Pierre Chifflier <chifflier@wzdftpd.net>
2025-01-30 11:05:45 +0100

    Add crate-level documentation and fix tests

--- x509_lint/src/lib.rs
+++ x509_lint/src/lib.rs
@@ -1,4 +1,33 @@
 //! X.509 Certificate and CRL linter
+//!
+//! This crate provides two kinds of lints: [`CertificateLint`] for X.509 Certificates,
+//! and [`CRLLint`] for X.509 Certificate Revocation Lists.
+//!
+//! Lints are simple functions receiving a parsed object, and returning a [`LintResult`].
+//!
+//! # Running lints
+//!
+//! To run lints, either define and call the lints directly, or use a lint registry to
+//! store lint definitions and functions.
+//!
+//! This crate provides default registries for certificates and CRLs.
+//!
+//! For example, to get the default registry for RFC5280 lints on X.509 Certificates,
+//! use the following:
+//!
+//! ```rust
+//! use x509_lint::*;
+//! use x509_lint::x509_parser::prelude::X509Certificate;
+//!
+//! fn check_certificate(x509: &X509Certificate<'_>) {
+//!     let registry = rfc_lints();
+//!     let _results = registry.run_lints(x509);
+//!     // use results
+//! }
+//! ```
+//!
+//! `run_lints` returns a list of [`LintDefinition`] and [`LintResult`].
+//! The lifetime of definitions is the lifetime of the registry containing them.
 
 #![deny(/*missing_docs,*/
     unstable_features,
